3776308e805a4eaeb72147017303da2daa735eee
Andrey L <fanton96mailru@gmail.com>
2025-04-26 23:56:34 +0500

    improve zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -4,33 +4,42 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import { promisify } from "util";
 import { pipeline } from "stream";
-import { createGzip } from 'zlib';
+import { createGzip } from "zlib";
 
 const compressPipeline = promisify(pipeline);
 
 const compress = async () => {
-    const filePath = join(
-        dirname(fileURLToPath(import.meta.url)),
-        "files",
-        "fileToCompress.txt"
-      );
-      const compressedFilePath = join(
-        dirname(fileURLToPath(import.meta.url)),
-        "files",
-        "archive.gz"
-      );
-    
-      const readStream = createReadStream(filePath);
-      const writeStream = createWriteStream(compressedFilePath);
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCompress.txt"
+  );
+  const compressedFilePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "archive.gz"
+  );
 
-      const gzip = createGzip();
-    
-      try {
-        await compressPipeline(readStream, gzip, writeStream);
-        await unlink(filePath);
-      } catch (err) {
-        console.error(err);
-      }
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(compressedFilePath);
+
+  const gzip = createGzip();
+
+  readStream.on("error", (err) => {
+    gzip.destroy();
+    writeStream.destroy();
+  });
+
+  try {
+    await compressPipeline(readStream, gzip, writeStream);
+    await unlink(filePath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error(`Error: The file ${filePath} does not exist.`);
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -4,33 +4,44 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import { promisify } from "util";
 import { pipeline } from "stream";
-import { createGunzip } from 'zlib';
+import { createGunzip } from "zlib";
 
 const decompressPipeline = promisify(pipeline);
 
 const decompress = async () => {
-    const filePath = join(
-        dirname(fileURLToPath(import.meta.url)),
-        "files",
-        "archive.gz"
-      );
-      const decompressedFilePath = join(
-        dirname(fileURLToPath(import.meta.url)),
-        "files",
-        "fileToCompress.txt"
-      );
-    
-      const readStream = createReadStream(filePath);
-      const writeStream = createWriteStream(decompressedFilePath);
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "archive.gz"
+  );
+  const decompressedFilePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCompress.txt"
+  );
 
-      const gunzip = createGunzip();
-    
-      try {
-        await decompressPipeline(readStream, gunzip, writeStream);
-        await unlink(filePath);
-      } catch (err) {
-        console.error(err);
-      }
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(decompressedFilePath);
+
+  const gunzip = createGunzip();
+
+  readStream.on("error", (err) => {
+    gunzip.destroy();
+    writeStream.destroy();
+  });
+
+  try {
+    await decompressPipeline(readStream, gunzip, writeStream);
+    await unlink(filePath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error(
+        `Error: The file ${filePath} does not exist. Please compress file first.`
+      );
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await decompress();
