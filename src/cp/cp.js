6b37c668eea637953cd29d2ffa9026cb2967947f
Andrey L <fanton96mailru@gmail.com>
2025-04-27 01:16:23 +0500

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,34 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { spawn } from "child_process";
+
+const spawnChildProcess = async (args = []) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const childProcessPath = join(__dirname, "files", "script.js");
+
+  const childProcess = spawn("node", [childProcessPath, ...args]);
+
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
+
+  process.stdin.on("data", (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  return new Promise((resolve) => {
+    childProcess.on("close", (code) => {
+      process.stdin.pause();
+      resolve(code);
+    });
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const childProcessExitCode = await spawnChildProcess([
+  "someArgument1",
+  "someArgument2",
+]);
+console.log(`Child process exited with code: ${childProcessExitCode}`);
