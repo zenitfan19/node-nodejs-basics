bc53ccce38736a1eba729099c48a6135c8abb227
Andrey L <fanton96mailru@gmail.com>
2025-04-27 00:52:17 +0500

    feat: implement worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import { cpus } from "os";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { Worker } from "worker_threads";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numberOfCPUs = cpus().length;
+  const workerPath = join(__dirname, "worker.js");
+  const workerPromises = [];
+
+  console.log(`Number of CPUs: ${numberOfCPUs}`);
+
+  for (let i = 0; i < numberOfCPUs; i++) {
+    workerPromises.push(
+      new Promise((resolve) => {
+        const worker = new Worker(workerPath);
+        const dataToSend = 10 + i;
+
+        worker.on("message", (result) => {
+          worker.terminate();
+
+          if (result.error) {
+            resolve({
+              status: "error",
+              data: null,
+            });
+          } else {
+            resolve({
+              status: "resolved",
+              data: result,
+            });
+          }
+        });
+
+        worker.postMessage(dataToSend);
+      })
+    );
+  }
+
+  const resultsArray = await Promise.all(workerPromises);
+
+  console.log(resultsArray);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (result) => {
+  parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.on("message", (n) => {
+  try {
+    const result = nthFibonacci(n);
+    sendResult(result);
+  } catch (error) {
+    parentPort.postMessage({ error: error.message });
+  }
+});
